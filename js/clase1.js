a8b08147054a8e30742f0411096f5d8247ed3a30
Daniel Hernandez Torres <daedheto@gmail.com>
2021-10-29 20:44:03 -0300

    primer desafio

---
+++ js/clase1.js
@@ -0,0 +1,13 @@
+let nombre = prompt("ingresar nombre");
+let edad = prompt("ingresa tu edad");
+let nacionalidad = prompt("ingresar pais de nacimiento");
+
+alert(
+  "Hola soy " +
+    nombre +
+    ", soy de " +
+    nacionalidad +
+    " y tengo " +
+    edad +
+    " años"
+);
